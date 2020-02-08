0a05b44f86d8353b72548c06770eb5fbb93e071f
Tiago Douglas <tiagodouglas.c@gmail.comm>
2020-02-08 03:28:26 -0300

    Fixing collider

--- src/Weapons/Projectile.cs
+++ src/Weapons/Projectile.cs
@@ -3,44 +3,42 @@ using Godot;
 public class Projectile : RigidBody2D
 {
 
-	[Export]
-	private float Speed = 700.0f;
-	private Timer _timer;
+    [Export]
+    private float Speed = 700.0f;
+    private Timer _timer;
 
 
-	public override void _Ready()
-	{
-		_timer = (Timer)GetNode("Timer");
-		_timer.Start();
+    public override void _Ready()
+    {
+        _timer = (Timer)GetNode("Timer");
+        _timer.Start();
 
-	}
+    }
 
-	public override void _PhysicsProcess(float delta)
-	{
-		if (_timer.IsStopped())
-			QueueFree();
+    public override void _PhysicsProcess(float delta)
+    {
+        if (_timer.IsStopped())
+            QueueFree();
 
-		var colliders = GetCollidingBodies();
+        var colliders = GetCollidingBodies();
 
-		if (colliders != null)
-			foreach (var collider in colliders) 
-			{
-				var obj = (CollisionObject2D)collider;
-				if (obj.IsInGroup("Enemy"))
-				{
-					obj.QueueFree();
-					QueueFree();
-				}
-			}
-				
-	}
+        foreach (CollisionObject2D collider in colliders)
+        {
+            if (collider.IsInGroup("Enemy"))
+            {
+                collider.QueueFree();
+                QueueFree();
+            }
+        }
 
-	public void ShootAtMousePosition(Vector2 startPosition)
-	{
-		GlobalPosition = startPosition;
-		var direction = (GetGlobalMousePosition() - startPosition).Normalized();
-		LinearVelocity = direction * Speed;
-	}
+    }
+
+    public void ShootAtMousePosition(Vector2 startPosition)
+    {
+        GlobalPosition = startPosition;
+        var direction = (GetGlobalMousePosition() - startPosition).Normalized();
+        LinearVelocity = direction * Speed;
+    }
 
 }
 
